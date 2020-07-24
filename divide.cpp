070125f78765bae732e98c3e580ef017f9701d35
Fabricio-Jose <33519863+Fabricio-Jose@users.noreply.github.com>
2020-07-23 19:32:13 -0500

    Update divide.cpp

--- divide.cpp
+++ divide.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 
+/*
+ g++ divide.cpp -o divide.o
+./divide.o<prueba.txt>salida.txt
+ */
 int max_2(int x, int y){
     if (x>y)
         return x;
@@ -21,112 +25,103 @@ int max_3(int x, int y, int z){
 
 int acumulado(int **A,int f_sub,int c_sup, int f_inf,int c_inf, bool plano)
 {
-	if(f_sub==f_inf&&c_sup==c_inf)	{
+	if(f_sub==f_inf && c_sup==c_inf)	{
 		return max(0,A[f_sub][c_sup]);
 	}
 	if(plano){
-		if(c_sup!=c_inf){
-			int q=(c_sup+c_inf)/2;
+	if(c_sup!=c_inf){
+		int q=(c_sup+c_inf)/2;
 
-			int maxizq=acumulado(A,f_sub,c_sup,f_inf,q,!plano);
-			int maxder=acumulado(A,f_sub,q+1,f_inf,c_inf,!plano);
+		int maxizq=acumulado(A,f_sub,c_sup,f_inf,q,!plano);
+		int maxder=acumulado(A,f_sub,q+1,f_inf,c_inf,!plano);
 
-			int max2izq=A[f_sub][q];
-			int max2der=A[f_sub][q+1];
-			int maxcruz=A[f_sub][q];
-			for(int g=1;g<(f_inf+f_sub+2);g++){
-				for(int k=f_sub;k<(f_inf-g+2);k++){
-					int suma=0;
-					for(int l=k;l<k+g;l++){
-						suma=suma + A[l][q];
-                        //cout<<suma<<endl
-
-					}
-					max2izq=suma;
-					for(int i =q-1;i>(c_sup-1);i--){
-						int temporal=0;
-						for(int l=k;l<k+g;l++){
-							temporal=temporal+A[l][i];
-                           //cout<<temporal<<endl
-
-						}
-						suma=suma+temporal;
-						max2izq=max_2(max2izq,suma);
-					}
-					suma =0;
-					for(int l=k;l<k+g;l++){
-						suma=suma+A[l][q+1];
-                        //cout<<suma<<endl                        
-                    }
-					max2der=suma;
-					for(int i =q+2;i<(c_inf+1);i++){
-						int temporal=0;
-						for(int l=k;l<k+g;l++)
-							temporal=temporal+A[l][i];
-                            //cout<<temporal<<endl
-						suma=suma+temporal;
-						max2der=max_2(max2der,suma);
-					}
-					maxcruz=max_2(maxcruz,max2izq+max2der);
-				}
+		int max2izq=A[f_sub][q];
+		int max2der=A[f_sub][q+1];
+		int maxcruz=A[f_sub][q];
+		for(int g=1;g<(f_inf+f_sub+2);g++){
+		for(int k=f_sub;k<(f_inf-g+2);k++){
+		    int suma=0;
+		    for(int l=k;l<k+g;l++){
+		 	suma=suma + A[l][q];
+			//cout<<suma<<endl
 			}
-			return max_3(maxder,maxizq,maxcruz);
-		}
-		else{
-			return acumulado(A,f_sub,c_sup,f_inf,c_inf,!plano);
-		}
-    }
-	else{
-		if(f_sub!=f_inf){
-            
-			int q=(f_sub+f_inf)/2;
-			int maxsup=acumulado(A,f_sub,c_sup,q,c_inf,!plano);
-			int maxinf=acumulado(A,q+1,c_sup,f_inf,c_inf,!plano);
-
-			int max2sup=A[q][c_sup];
-			int max2inf=A[q+1][c_sup];
-			int maxcruz=A[q][c_sup];
-			for(int g=1;g<(c_inf+c_sup+2);g++){
-				for(int k=c_sup;k<(c_inf-g+2);k++){
-					int suma=0;
-					for(int l=k;l<k+g;l++)
-						suma+=A[q][l];
-                        //cout<<suma<<endl                    
-					max2sup=suma;
-
-					for(int i =q-1;i>(f_sub-1);i--){
-						int temporal=0;
-						for(int l=k;l<k+g;l++)
-							temporal+=A[i][l];
-                            //cout<<suma<<endl                        
-						suma=suma+temporal;
-						max2sup=max_2(max2sup,suma);
-					}
-
-					suma=0;
-
-					for(int r=k;r<k+g;r++)
-						suma+=A[q+1][r];
-					max2inf=suma;
-
-					for(int i =q+2;i<(f_inf+1);i++){
-						int temporal=0;
-						for(int l=k;l<k+g;l++)
-							temporal+=A[i][l];
-						suma=suma+temporal;
-						max2inf=max_2(max2inf,suma);
-					}
-					
-					maxcruz=max_2(maxcruz,max2inf+max2sup);
-				}
-
+			max2izq=suma;
+			for(int i =q-1;i>(c_sup-1);i--){
+		            int temporal=0;
+			    for(int l=k;l<k+g;l++){
+				temporal=temporal+A[l][i];
+			   //cout<<temporal<<endl
+			    }
+			    suma=suma+temporal;
+			    max2izq=max_2(max2izq,suma);
 			}
-			return max_3(maxinf,maxsup,maxcruz);
-		}
-		else{
-			return acumulado(A,f_sub,c_sup,f_inf,c_inf,!plano);
+			suma =0;
+			for(int l=k;l<k+g;l++){
+				suma=suma+A[l][q+1];
+			//cout<<suma<<endl                        
+			}
+			max2der=suma;
+			for(int i =q+2;i<(c_inf+1);i++){
+			    int temporal=0;
+			    for(int l=k;l<k+g;l++)
+			 	temporal=temporal+A[l][i];
+	    //cout<<temporal<<endl
+			suma=suma+temporal;
+			max2der=max_2(max2der,suma);
+			}
+			maxcruz=max_2(maxcruz,max2izq+max2der);
 		}
 	}
+            return max_3(maxder,maxizq,maxcruz);
+	}
+	else{
+	    return acumulado(A,f_sub,c_sup,f_inf,c_inf,!plano);
+	}
+    }
+	else{
+	if(f_sub!=f_inf){
+            int q=(f_sub+f_inf)/2;
+	    int maxsup=acumulado(A,f_sub,c_sup,q,c_inf,!plano);
+	    int maxinf=acumulado(A,q+1,c_sup,f_inf,c_inf,!plano);
+	    int max2sup=A[q][c_sup];
+	    int max2inf=A[q+1][c_sup];
+	    int maxcruz=A[q][c_sup];
+	    for(int g=1;g<(c_inf+c_sup+2);g++){
+	    for(int k=c_sup;k<(c_inf-g+2);k++){
+		int suma=0;
+		for(int l=k;l<k+g;l++)
+	            suma+=A[q][l];
+		//cout<<suma<<endl                    
+		max2sup=suma;
+		for(int i =q-1;i>(f_sub-1);i--){
+                    int temporal=0;
+		    for(int l=k;l<k+g;l++)
+		        temporal+=A[i][l];
+		    //cout<<suma<<endl                        
+		suma=suma+temporal;
+		max2sup=max_2(max2sup,suma);
+		}
+		suma=0;
+		for(int r=k;r<k+g;r++)
+			suma+=A[q+1][r];
+		max2inf=suma;
+
+		for(int i =q+2;i<(f_inf+1);i++){
+			int temporal=0;
+		for(int l=k;l<k+g;l++)
+	            temporal+=A[i][l];
+		suma=suma+temporal;
+		max2inf=max_2(max2inf,suma);
+		}
+		maxcruz=max_2(maxcruz,max2inf+max2sup);
+		}
+		}
+		return max_3(maxinf,maxsup,maxcruz);
+		}
+	else{
+		return acumulado(A,f_sub,c_sup,f_inf,c_inf,!plano);
+	}
+}
 }
 
 
@@ -158,3 +153,4 @@ int main(){
 //    cout<<"Cui: 20131661"<<endl;
 	return 0;
 }
+
